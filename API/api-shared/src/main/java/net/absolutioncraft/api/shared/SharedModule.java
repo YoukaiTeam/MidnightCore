ddc05805b59b9a26d7ff9ce2de5d88b7e6538126
mel0nsandia <squidnetworkes@gmail.com>
2020-08-03 00:37:01 -0400

    Added ServerOnlineStorage & RedisMessager binds to module configuration.

--- API/api-shared/src/main/java/net/absolutioncraft/api/shared/SharedModule.java
+++ API/api-shared/src/main/java/net/absolutioncraft/api/shared/SharedModule.java
@@ -6,6 +6,10 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
+import net.absolutioncraft.api.shared.online.ServerOnlineStorage;
+import net.absolutioncraft.api.shared.online.provider.OnlineProvider;
+import net.absolutioncraft.api.shared.redis.RedisMessager;
+import net.absolutioncraft.api.shared.redis.messager.Messager;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,11 +23,14 @@ public final class SharedModule extends AbstractModule {
     public void configure() {
         bind(ExecutorService.class).to(ListeningExecutorService.class).in(Scopes.SINGLETON);
         bind(ListeningExecutorService.class).toInstance(MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors())));
+        bind(OnlineProvider.class).to(ServerOnlineStorage.class).in(Scopes.SINGLETON);
 
         bind(Gson.class).toProvider(() -> new GsonBuilder()
                 .serializeNulls()
                 .enableComplexMapKeySerialization()
                 .setPrettyPrinting()
                 .create()).in(Scopes.SINGLETON);
+
+        bind(Messager.class).to(RedisMessager.class).in(Scopes.SINGLETON);
     }
 }
