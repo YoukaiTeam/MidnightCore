b589f1b8e534365cd14d1a3d947d3aac7830a5ef
mel0nsandia <squidnetworkes@gmail.com>
2020-08-28 22:28:55 -0400

    Added better throwable printing.

--- API/api-shared/src/main/java/net/absolutioncraft/api/shared/concurrent/Callback.java
+++ API/api-shared/src/main/java/net/absolutioncraft/api/shared/concurrent/Callback.java
@@ -9,8 +9,6 @@ public interface Callback<T> {
      */
     default void handleException(final Throwable throwable) {
         System.out.println("Internal error occurred while handling callback.");
-        for (StackTraceElement stackTraceElement : throwable.getStackTrace()) {
-            System.out.println(stackTraceElement.getClassName() + "|" + stackTraceElement.getLineNumber());
-        }
+        throwable.printStackTrace();
     }
 }
