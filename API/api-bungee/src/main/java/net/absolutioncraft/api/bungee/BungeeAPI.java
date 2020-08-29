1165b9c31ce4d2404e54249a8c41c1d50348e8fd
mel0nsandia <squidnetworkes@gmail.com>
2020-08-28 22:33:30 -0400

    Created BungeeAPI module template.

---
+++ API/api-bungee/src/main/java/net/absolutioncraft/api/bungee/BungeeAPI.java
@@ -0,0 +1,11 @@
+package net.absolutioncraft.api.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * @author MelonDev
+ * @since 0.0.1
+ */
+public final class BungeeAPI extends Plugin {
+
+}
